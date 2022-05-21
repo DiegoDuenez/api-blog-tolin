db9a1c2b6576a94a608172de539452907dcb1347
Your Name <you@example.com>
2022-05-21 18:06:12 -0500

    Auth

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\Authentication;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,3 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('login',[Authentication::class,'login']);
+Route::middleware('auth:sanctum')->group(function(){
+Route::post('logout',[Authentication::class,'logout']);
+Route::get('user',function(Request $request){
+    return $request->user();
+});
+
+});
+Route::post('register',[Authentication::class,'register']);
